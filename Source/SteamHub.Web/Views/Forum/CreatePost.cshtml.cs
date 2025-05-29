d5f5cb4e424e23b4b2196f9eecec6e5d7df88a8b
Robert George Leustean <147397130+914RobbLeustean@users.noreply.github.com>
2025-05-29 19:03:40 +0300

    Add files via upload

---
+++ Source/SteamHub.Web/Views/Forum/CreatePost.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SteamHub.Web.Views.Forum
+{
+    public class CreatePostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
