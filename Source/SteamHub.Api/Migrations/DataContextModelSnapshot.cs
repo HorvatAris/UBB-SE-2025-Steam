c2a594728589ed8ebdd87563274153ccb1890edf
HorvatAris <calin.horvat@stud.ubbcluj.ro>
2025-05-29 16:57:28 +0300

    migr

--- Source/SteamHub.Api/Migrations/DataContextModelSnapshot.cs
+++ Source/SteamHub.Api/Migrations/DataContextModelSnapshot.cs
@@ -3,7 +3,6 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SteamHub.Api.Context;
 
@@ -12,11 +11,9 @@ using SteamHub.Api.Context;
 namespace SteamHub.Api.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20250529125536_MainMigration")]
-    partial class MainMigration
+    partial class DataContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
