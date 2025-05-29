f9fc27f5fc783ed40b69b9b53d141b2e11fb878e
halip-sonia <sonia.halip@stud.ubbcluj.ro>
2025-05-29 16:27:21 +0300

    fixing friend req

---
+++ Source/SteamHub/Pages/FriendRequestsPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SteamHub.ViewModels;
+using Microsoft.UI.Xaml.Controls;
+using SteamHub.ViewModels;
+
+namespace SteamHub.Pages
+{
+    public sealed partial class FriendRequestsPage : Page
+    {
+        public ProfileViewModel ProfileViewModel { get; set; }
+        public FriendRequestViewModel FriendRequestViewModel { get; set; }
+
+        public FriendRequestsPage(ProfileViewModel profileViewModel, FriendRequestViewModel friendRequestViewModel)
+        {
+            // Get view models from DI container (same as in ProfileControl)
+            FriendRequestViewModel = friendRequestViewModel;
+
+            ProfileViewModel = profileViewModel;
+
+            this.InitializeComponent();
+        }
+    }
+}
