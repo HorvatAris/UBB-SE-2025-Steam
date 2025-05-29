5fc2a90ed9b847e5e2302be38c63ec85ae9b5fb9
Mara Ielciu <146743027+maraielciu1@users.noreply.github.com>
2025-05-29 14:56:35 +0300

    gataaa

--- Source/SteamHub.Api/Migrations/20250529114334_MainMigration.Designer.cs
+++ Source/SteamHub.Api/Migrations/20250529114334_MainMigration.Designer.cs
@@ -12,7 +12,7 @@ using SteamHub.Api.Context;
 namespace SteamHub.Api.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20250529053724_MainMigration")]
+    [Migration("20250529114334_MainMigration")]
     partial class MainMigration
     {
         /// <inheritdoc />
