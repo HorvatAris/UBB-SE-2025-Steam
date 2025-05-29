a0bdfcf76d89b879d1fc5c35a86fed10bb07fa0e
Robert George Leustean <147397130+914RobbLeustean@users.noreply.github.com>
2025-05-29 19:01:58 +0300

    viewmodels for forum web

---
+++ Source/SteamHub.Web/ViewModels/CreatePostViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SteamHub.Web.ViewModels
+{
+    public class CreatePostViewModel
+    {
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Content is required")]
+        public string Content { get; set; } = string.Empty;
+
+        public int? GameId { get; set; }
+    }
+}
---
+++ Source/SteamHub.Web/ViewModels/PostDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SteamHub.ApiContract.Models;
+using System.Collections.Generic;
+
+namespace SteamHub.Web.ViewModels
+{
+    public class PostDetailViewModel
+    {
+        public ForumPost Post { get; set; } = new ForumPost();
+        public List<ForumComment> Comments { get; set; } = new List<ForumComment>();
+        public int CurrentUserId { get; set; }
+    }
+}
