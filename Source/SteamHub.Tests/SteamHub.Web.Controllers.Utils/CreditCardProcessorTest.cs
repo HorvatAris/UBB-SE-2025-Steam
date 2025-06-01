f497e922a4829c928687675fcd6b073fab3111fb
andrei-manta <andreimanta100@gmail.com>
2025-06-01 19:29:02 +0300

    Uploading a minimal batch of tests

---
+++ Source/SteamHub.Tests/SteamHub.Web.Controllers.Utils/CreditCardProcessorTest.cs
@@ -0,0 +1,123 @@
+﻿namespace SteamHub.Tests.Utils;
+using Xunit;
+
+public class CreditCardProcessorTest
+{
+    private const string CorrectCardNumber = "1234567891012";
+    private const string CorrectExpirationDate = "12/30";
+    private const string Whitespace = " ";
+    private const string CorrectCvv = "999";
+    private const string CorrectName = "TEST";
+
+    private readonly CreditCardProcessor cardProcessor = new CreditCardProcessor();
+
+    [Fact]
+    public async Task ProcessPaymentAsync_WithAllValidInputs_ReturnsTrue()
+    {
+        var result = await cardProcessor.ProcessPaymentAsync(CorrectCardNumber, CorrectExpirationDate, CorrectCvv, CorrectName);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task ProcessPaymentAsync_WithWhitespaceCardNumber_ReturnsFalse()
+    {
+        var result = await cardProcessor.ProcessPaymentAsync(Whitespace, CorrectExpirationDate, CorrectCvv, CorrectName);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task ProcessPaymentAsync_WithShortCardNumber_ReturnsFalse()
+    {
+        string minimumCardNumberLengthNotMet = "123";
+
+        var result = await cardProcessor.ProcessPaymentAsync(minimumCardNumberLengthNotMet, CorrectExpirationDate, CorrectCvv, CorrectName);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task ProcessPaymentAsync_WithLongCardNumber_ReturnsFalse()
+    {
+        string maximumCardNumberLengthExceeded = "12345678910121234567891012";
+
+        var result = await cardProcessor.ProcessPaymentAsync(maximumCardNumberLengthExceeded, CorrectExpirationDate, CorrectCvv, CorrectName);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task ProcessPaymentAsync_WithWhitespaceExpirationDate_ReturnsFalse()
+    {
+        var result = await cardProcessor.ProcessPaymentAsync(CorrectCardNumber, Whitespace, CorrectCvv, CorrectName);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task ProcessPaymentAsync_WithIncorrectFormatExpirationDate_ReturnsFalse()
+    {
+        string incorrectFormatExpirationDate = "222";
+
+        var result = await cardProcessor.ProcessPaymentAsync(CorrectCardNumber, incorrectFormatExpirationDate, CorrectCvv, CorrectName);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task ProcessPaymentAsync_WithWhitespaceCvv_ReturnsFalse()
+    {
+        var result = await cardProcessor.ProcessPaymentAsync(CorrectCardNumber, CorrectExpirationDate, Whitespace, CorrectName);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task ProcessPaymentAsync_WithPastYearExpirationDate_ReturnsFalse()
+    {
+        string pastYearExpirationDate = "12/10";
+
+        var result = await cardProcessor.ProcessPaymentAsync(CorrectCardNumber, pastYearExpirationDate, CorrectCvv, CorrectName);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task ProcessPaymentAsync_WithPastMonthExpirationDate_ReturnsFalse()
+    {
+        string pastMonthExpirationDate = "1/25";
+
+        var result = await cardProcessor.ProcessPaymentAsync(CorrectCardNumber, pastMonthExpirationDate, CorrectCvv, CorrectName);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task ProcessPaymentAsync_WithIncorrectCvv_ReturnsFalse()
+    {
+        string incorrectCvv = "99999";
+
+        var result = await cardProcessor.ProcessPaymentAsync(CorrectCardNumber, CorrectExpirationDate, incorrectCvv, CorrectName);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task ProcessPaymentAsync_WithWhitespaceName_ReturnsFalse()
+    {
+        var result = await cardProcessor.ProcessPaymentAsync(CorrectCardNumber, CorrectExpirationDate, CorrectCvv, Whitespace);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task ProcessPaymentAsync_WithInvalidName_ReturnsFalse()
+    {
+        string incorrectName = "123";
+
+        var result = await cardProcessor.ProcessPaymentAsync(CorrectCardNumber, CorrectExpirationDate, CorrectCvv, incorrectName);
+
+        Assert.False(result);
+    }
+}
